55785ade0bc6900d4140a23246160b6eeb46fca4
rafterminador <ferrafterminador@gmail.com>
2024-02-01 19:43:07 -0600

    adding initiall structure

---
+++ src/test/java/com/theonlyfer/springbootmongoapp/SpringbootMongoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theonlyfer.springbootmongoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
